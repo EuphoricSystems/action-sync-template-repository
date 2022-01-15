d1a00aa62bbc046113bfb956d06b79fce7930160
Chris Young <youngcm2@gmail.com>
2022-01-15 00:08:56 -0600

    feat: switched to GraphQL

--- action/lib/repos.js
+++ action/lib/repos.js
@@ -1,82 +1,128 @@
 /* eslint-disable camelcase */
 
 // node modules
-import { inspect } from 'util'
+import { inspect } from "util";
 
 // packages
-import core from '@actions/core'
-import github from '@actions/github'
-import micromatch from 'micromatch'
+import core from "@actions/core";
+import github from "@actions/github";
+import micromatch from "micromatch";
+import {graphql} from "@octokit/graphql";
+// import qraphql, { graphql } from "@octokit/graphql";
 
 const mediaType = {
-  previews: [
-    'baptiste'
-  ]
-}
+  previews: ["baptiste"],
+};
 
 export default async function (octokit, options) {
   // construct this template repo full name
-  const full_name = `${github.context.repo.owner}/${github.context.repo.repo}`
+  const full_name = `${github.context.repo.owner}/${github.context.repo.repo}`;
 
   // determine type
-  const { data: { is_template, owner: { type } } } = await octokit.request('GET /repos/{owner}/{repo}', {
+  const {
+    data: {
+      is_template,
+      owner: { type },
+    },
+  } = await octokit.request("GET /repos/{owner}/{repo}", {
     ...github.context.repo,
-    mediaType
-  })
+    mediaType,
+  });
 
   // exit early
   if (!is_template) {
-    core.warning('action executed on non template repository')
-    process.exit(0)
+    core.warning("action executed on non template repository");
+    process.exit(0);
   }
 
-  const api = (type === 'User') ? 'GET /users/{username}/repos' : 'GET /orgs/{org}/repos'
+  // const api =
+  //   type === "User" ? "GET /users/{username}/repos" : "GET /orgs/{org}/repos";
 
-  const all = await octokit.paginate(api, {
-    username: github.context.repo.owner,
-    org: github.context.repo.owner,
-    per_page: 100,
-    mediaType
-  })
+  const query = `
+  query ($cursor: String) {
+  organization(login: "swimlane") {
+    repositories(first: 100, after: $cursor, orderBy: {field: CREATED_AT, direction: DESC}) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        name
+        isArchived
+        templateRepository {
+          name
+          nameWithOwner
+        }
+      }
+    }
+  }
+}
 
-  const repositories = all.filter(repo => repo.archived === false) // only include non-archived repos
+  `;
 
-  core.debug(`repo owner type is "${type}" with ${repositories.length} repositories`)
+  async function fetchRepos({results, cursor} = {results: []}) {
+    const { organization: {repositories}} = await octokit.graphql(query, {cursor}    )
+    results.push(...repositories.nodes)
+
+    if(repositories.pageInfo.hasNextPage){
+      // await fetchRepos({results, cursor: repositories.pageInfo.endCursor})
+    }
+
+    return results
+  }
+  const all = await fetchRepos()
+  // const all = await octokit.paginate(api, {
+  //   username: github.context.repo.owner,
+  //   org: github.context.repo.owner,
+  //   per_page: 100,
+  //   mediaType,
+  // });
+
+  const repositories = all.filter((repo) => repo.isArchived === false); // only include non-archived repos
+
+  core.debug(
+    `repo owner type is "${type}" with ${repositories.length} repositories`
+  );
 
   // find all repos that mark this template as their source
   let dependents = repositories
-    .filter(repo => repo.template_repository && repo.template_repository.full_name === full_name)
-    .map(repo => repo.name)
+    .filter(
+      (repo) =>
+        repo.templateRepository &&
+        repo.templateRepository.nameWithOwner === full_name
+    )
+    .map((repo) => repo.name);
 
   // run filter
-  if (options.dependents.length > 0) dependents = micromatch(dependents, options.dependents)
+  if (options.dependents.length > 0)
+    dependents = micromatch(dependents, options.dependents);
 
-  core.info(`found ${dependents.length} repositories marked as dependents`)
-  if (dependents.length > 0) core.debug(inspect(dependents))
+  core.info(`found ${dependents.length} repositories marked as dependents`);
+  if (dependents.length > 0) core.debug(inspect(dependents));
 
-  let additional = []
+  let additional = [];
 
   // TODO run a schema validation for config
 
   if (options.additional.length > 0) {
     additional = repositories
       // exclude the template repo itself
-      .filter(repo => repo.full_name !== full_name)
+      .filter((repo) => repo.full_name !== full_name)
       // create list of all names
-      .map(repo => repo.name)
+      .map((repo) => repo.name);
 
     // any special ones to include?
-    additional = micromatch(additional, options.additional)
+    additional = micromatch(additional, options.additional);
 
-    core.info(`found ${additional.length} repositories marked as additional`)
-    if (additional.length > 0) core.debug(inspect(additional))
+    core.info(`found ${additional.length} repositories marked as additional`);
+    if (additional.length > 0) core.debug(inspect(additional));
   }
 
   // combine them & remove duplicates
-  const final = [...new Set([...dependents, ...additional])]
+  const final = [...new Set([...dependents, ...additional])];
 
-  core.info(`final list of repos includes ${final.length} repositories`)
-  if (final.length > 0) core.debug(inspect(final))
+  core.info(`final list of repos includes ${final.length} repositories`);
+  if (final.length > 0) core.debug(inspect(final));
 
-  return final
+  return final;
 }
