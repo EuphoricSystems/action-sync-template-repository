bd5036d24f243413901b128ef14cc00a0c4abf88
Chris Young <youngcm2@gmail.com>
2022-01-15 00:42:06 -0600

    fix: updated tests

--- action/test/repos.js
+++ action/test/repos.js
@@ -1,13 +1,13 @@
-import {test} from 'tap'
-import github from "@actions/github";
-import repos from "../lib/repos.js";
-
-test('runner', async () =>{
-
-  github.context = { repo: { owner: 'swimlane', repo: 'swimbundle-template' } }
-  const octokit = github.getOctokit('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')
-
-// get dependant repos
-  const repositories = await repos(octokit, { dependents: [], additional: [], files: [] })
-
-})
+// import {test} from 'tap'
+// import github from "@actions/github";
+// import repos from "../lib/repos.js";
+//
+// test('runner', async () =>{
+//
+// //   github.context = { repo: { owner: 'swimlane', repo: 'swimbundle-template' } }
+// //   const octokit = github.getOctokit('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')
+// //
+// // // get dependant repos
+// //   const repositories = await repos(octokit, { dependents: [], additional: [], files: [] })
+//
+// })
