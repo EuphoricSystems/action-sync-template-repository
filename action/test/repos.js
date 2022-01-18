b334ba7dd66b4c484f91e0ce285567764a770dc0
Chris Young <youngcm2@gmail.com>
2022-01-18 13:41:38 -0600

    fix: updated readme and action.yml

---
+++ action/test/repos.js
@@ -0,0 +1,13 @@
+import {test} from 'tap'
+import github from "@actions/github";
+import repos from "../lib/repos.js";
+
+test('runner', async () =>{
+
+  github.context = { repo: { owner: '', repo: '' } }
+  const octokit = github.getOctokit('')
+
+// get dependant repos
+  const repositories = await repos(octokit, { dependents: [], additional: [], files: [] })
+
+})
